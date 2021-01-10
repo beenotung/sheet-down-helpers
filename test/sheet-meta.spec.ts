41a03919c78372db17ccff6042f1bdc7131cbc1b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-01-11 02:29:30 +0800

    tidy up code

--- test/sheet-meta.spec.ts
+++ test/sheet-meta.spec.ts
@@ -8,10 +8,13 @@ describe('sheet-meta TestSuit', () => {
   it('should parse config', async function () {
     const xml = readFileSync('full.xml').toString()
     const sheetMeta = await parseSheetMeta(xml)
+
     expect(sheetMeta).not.undefined
+
     const { spreadsheet_id, worksheets } = sheetMeta
     expect(spreadsheet_id).not.empty
     expect(worksheets).not.empty
+
     const [worksheet] = worksheets
     expect(worksheet).not.undefined
     expect(worksheet.worksheet_id).not.undefined
